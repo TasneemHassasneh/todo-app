f38a0b02105f73b2ebb2596ea9d7abc77dd8212c
TasneemHassasneh <tasneemhassasneh890@gmail.com>
2023-09-16 11:44:13 +0000

    manage settings

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 import Home from './Components/Home/Home';
+import SettingsPage from './Components/Settings/Settings';
+import { SettingsProvider } from './context/SettingContext';
 
 export default class App extends React.Component {
   render() {
     return (
-      
-    <Home />
+      <>
+      <SettingsProvider>
+      <Home />
+    <SettingsPage />
+      </SettingsProvider>
+         </>
+ 
      
     );
   }
--- src/context/SettingContext.jsx
+++ src/context/SettingContext.jsx
@@ -1,41 +1,29 @@
 // SettingsContext.js
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 const SettingsContext = createContext();
 
 export const useSettings = () => useContext(SettingsContext);
 
 export const SettingsProvider = ({ children }) => {
-  const [displaySettings, setDisplaySettings] = useState({
-    itemsPerPage: 3,
-    hideCompletedItems: true,
-    defaultSortField: 'difficulty',
+  const [settings, setSettings] = useState({
+    itemsToShow: 3, 
+    showCompleted: true, 
   });
 
-  // Function to save settings to local storage
-  const saveSettingsToLocalStorage = () => {
-    localStorage.setItem('displaySettings', JSON.stringify(displaySettings));
-  };
-
-  // Function to retrieve settings from local storage on application load
-  const getSettingsFromLocalStorage = () => {
-    const storedSettings = localStorage.getItem('displaySettings');
-    if (storedSettings) {
-      setDisplaySettings(JSON.parse(storedSettings));
+  useEffect(() => {
+    const savedSettings = localStorage.getItem('settings');
+    if (savedSettings) {
+      setSettings(JSON.parse(savedSettings));
     }
-  };
+  }, []);
 
   useEffect(() => {
-    getSettingsFromLocalStorage();
-  }, []); // Run once on component mount
-
-  // Update local storage whenever displaySettings change
-  useEffect(() => {
-    saveSettingsToLocalStorage();
-  }, [displaySettings]);
+    localStorage.setItem('settings', JSON.stringify(settings));
+  }, [settings]);
 
   return (
-    <SettingsContext.Provider value={{ displaySettings, setDisplaySettings }}>
+    <SettingsContext.Provider value={{ settings, setSettings }}>
       {children}
     </SettingsContext.Provider>
   );
